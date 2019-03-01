37da55b1611ee34a5b33c8a3d3f53ecf72ffc46f
Michael Dilley <mike.dilley@gmail.com>
2019-03-01 14:55:29 -0600

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+let i = 0;
+
 function init() {
   // Write your JavaScript code inside the init() function
-
+  const main = document.querySelector('body');
+  main.addEventListener('keydown', function(event){
+    let key = parseInt(event.detail || event.which);
+    if(key === code[i]) {
+      i ++;
+      if(i === code.length) {
+        alert('You entered the code!');
+        i = 0;
+      }
+    } else {
+      i = 0;
+    }
+  });
 }
